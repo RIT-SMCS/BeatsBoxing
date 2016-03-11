b156bcc570caf41d3b085f39f7bae90ecc0d36c2
Matthew Fobare (RIT Student) <mf7753@ad.rit.edu>
2016-03-11 14:48:33 -0500

    Added enemy base stuff. Now has an enemy manager and basic enemy.

---
+++ BeatsBoxing/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : LineActor {
+
+	// Use this for initialization
+	void Awake () {
+        _health = 1;
+        _xVelocity = 1.0f;
+        _currentLane = 0;
+        Lane = _currentLane;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}  
+
+    protected override void DoAttackPattern()
+    {
+
+    }
+}
---
+++ BeatsBoxing/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyManager : MonoBehaviour {
+
+    List<Enemy> enemies;
+
+	// Use this for initialization
+	void Start() {
+        enemies = new List<Enemy>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void MakeEnemy(int laneNum)
+    {
+        Enemy temp = new Enemy();
+        temp.Lane = laneNum;
+        enemies.Add(temp);
+    }
+}
