719e94ad5fa017b6aed72cadeb7e101589e2b156
Christopher Mercado (RIT Student) <cdm4363@ad.rit.edu>
2016-03-31 13:40:57 -0400

    Added scrolling background and made lane dividers look nicer

---
+++ BeatsBoxing/Assets/BackgroundPreview.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class BackgroundPreview : MonoBehaviour {
+
+    public int Width, Height;
+    public Sprite TileSprite;
+    public bool ToUpdate = false;
+    // Use this for initialization
+    void OnValidate()
+    {
+        Width = Mathf.Max(1, Width);
+        Height = Mathf.Max(1, Height);
+
+        if (TileSprite == null) { return; }
+
+        if (ToUpdate)
+        {
+            //deletes all current children
+            var children = new List<GameObject>();
+            foreach (Transform child in transform) { children.Add(child.gameObject); }
+            children.ForEach(child => UnityEditor.EditorApplication.delayCall += () =>
+                {
+                    DestroyImmediate(child.gameObject);
+                });
+
+            //create new children in the correct locations
+            float deltaX = transform.position.x - 0.5f * (Width - 1) * TileSprite.bounds.size.x;
+            float deltaY = transform.position.y - 0.5f * (Height - 1) * TileSprite.bounds.size.y;
+            for (int r = 0; r < Width * 3; ++r)
+            {
+                for (int c = 0; c < Height; ++c)
+                {
+                    GameObject tile = new GameObject("Sprite " + r + ", " + c);
+                    SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
+                    renderer.sprite = TileSprite;
+                    renderer.sortingOrder = -10;
+                    tile.transform.position = new Vector3(deltaX + r * TileSprite.bounds.size.x, deltaY + c * TileSprite.bounds.size.y);
+                    tile.transform.parent = this.transform;
+
+                }
+            }
+        }
+        /**/
+    }
+}
---
+++ BeatsBoxing/Assets/BackgroundScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BackgroundPreview))]
+public class BackgroundScroll : MonoBehaviour {
+    public float ScrollSpeed;
+
+    private float tileSizeX;
+    private Vector3 startPosition;
+
+    static int ID = 0;
+	// Use this for initialization
+	void Awake () {
+        startPosition = transform.position;
+        BackgroundPreview bp = gameObject.GetComponent<BackgroundPreview>();
+        tileSizeX = bp.Width * bp.TileSprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * ScrollSpeed * ScoreManager.SpeedScale, tileSizeX);
+        transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
