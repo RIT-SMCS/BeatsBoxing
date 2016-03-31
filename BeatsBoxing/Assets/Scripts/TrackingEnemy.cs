fb8a4f0dcb18b9873eca2db06d3fe367c9df9646
Matthew Fobare (RIT Student) <mf7753@ad.rit.edu>
2016-03-31 13:55:08 -0400

    Tracking enemy now the default, changes lane with player until it gets close.

--- BeatsBoxing/Assets/Scripts/TrackingEnemy.cs
+++ BeatsBoxing/Assets/Scripts/TrackingEnemy.cs
@@ -3,26 +3,35 @@ using System.Collections;
 
 public class TrackingEnemy : Enemy {
 
-	// Use this for initialization
-	void Awake () {
+    private int attackRange;
+
+    // Use this for initialization
+    public override void Awake(){
+        base.Awake();
         _health = 1;
         _xVelocity = -1.0f;
         _currentLane = 0;
         Lane = _currentLane;
         startX = 3.0f;
         currentState = State.Tracking;
+        attackRange = 2;
     }
 	
 	// Update is called once per frame
 	public override void Update () {
+        if(currentState == State.Tracking)
+        {
+            this.Lane = player.GetComponent<Player>().Lane;
+        }
+        if(Vector3.Distance(this.transform.position, player.transform.position) < attackRange)
+        {
+            this.currentState = State.Attacking;
+        }
         base.Update();
 	}
 
     protected override void DoAttackPattern()
     {
-        if (currentState == State.Tracking)
-        {
-
-        }
+        base.DoAttackPattern();
     }
 }
