7ea81fc34b70a4cbae1bffca8570d4474a4f7dda
Sean Maraia (RIT Student) <spm3713@ad.rit.edu>
2016-04-05 13:41:51 -0400

    implemented knock back, and disabled movement and attacking when being knocked back.

--- BeatsBoxing/Assets/Scripts/Player.cs
+++ BeatsBoxing/Assets/Scripts/Player.cs
@@ -4,24 +4,33 @@ using System;
 
 public class Player : LaneActor
 {
-	private int attackTimer; 
-
+	private int attackTimer;
+    float age = 0;
+    bool knockingBack = false;
+    Vector3 baseScale;
+    Vector3 startingPos;
+    //Animator anim;
+    //int damagehash = Animator.StringToHash("Damaged");
 	void Awake()
     {
         XVelocity = -1.0f;
         Health = 5;
         Lane = 5;
-		attackTimer = -1; 
+		attackTimer = -1;
+        baseScale = transform.localScale;
+        startingPos = transform.position;
     }
 
     public override void Update()
     {
+        
+
         //PLAYER CONTROL HANDLING
-		if (Input.GetKeyDown(KeyCode.W))
+		if (Input.GetKeyDown(KeyCode.W) && !knockingBack)
         {
             Lane++;
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && !knockingBack)
         {
             Lane--;
         }
@@ -31,28 +40,55 @@ public class Player : LaneActor
 			//Debug.Log("ATTACK!"); 
 		}
 
-		//Keep the attack from persisting forever
-		if (attackTimer > 0) 
-		{
-			attackTimer--; 
-			if(attackTimer <= 0)
-			{
-				attackTimer = -1; 
-				this.gameObject.transform.GetChild (0).GetComponent<Attack> ().attacking = false; 
-				//Debug.Log("ATTACK STOP"); 
-			}
-		}
+        //Keep the attack from persisting forever
+        if (attackTimer > 0)
+        {
+            attackTimer--;
+            if (attackTimer <= 0)
+            {
+                attackTimer = -1;
+                this.gameObject.transform.GetChild(0).GetComponent<Attack>().attacking = false;
+                //Debug.Log("ATTACK STOP"); 
+            }
+        }
+
+        if (knockingBack)
+        {
+            Knockback();
+        }
     }
 
     public override void DoAttackPattern()
     {
-		this.gameObject.transform.GetChild (0).GetComponent<Attack> ().attacking = true; 
-		attackTimer = 42; 
+        if (!knockingBack)
+        {
+            this.gameObject.transform.GetChild(0).GetComponent<Attack>().attacking = true;
+            attackTimer = 42;
+        }
     }
 
     public void TakeDamage(int damage)
     {
         this.Health -= damage;        
         ScoreManager.Combo = 0;
+
+        //anim.SetTrigger(damagehash);
+        age = 0.0f;
+        this.knockingBack = true;
+        this.startingPos = transform.position;
+    }
+
+    void Knockback()
+    {
+        age += Time.deltaTime;
+        float duration = 1.0f;
+        float t = Mathf.Min(age, duration) / duration;
+        transform.position = Vector3.Lerp(startingPos, startingPos - new Vector3(2 ,0, 0), t);
+        float scaleFactor = 1 + Mathf.Sin(t * Mathf.PI);
+        transform.localScale = baseScale * scaleFactor; 
+        if (age > duration)
+        {
+            knockingBack = false;
+        }
     }
 }
