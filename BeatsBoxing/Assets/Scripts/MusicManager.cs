29d357281edc3f402173c554cf52dc161ef4d313
Sarah Bishop (RIT Student) <sbb8791@ad.rit.edu>
2016-04-08 13:53:43 -0400

    Started infrastructure for dynamic music, added sound effects

--- BeatsBoxing/Assets/Scripts/MusicManager.cs
+++ BeatsBoxing/Assets/Scripts/MusicManager.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class MusicManager : MonoBehaviour {
 
+	public AudioClip startMusic; //the initial track
+	public AudioSource source; //the audioSource
+	float musicVol; //the music volume
+
 	// Use this for initialization
 	void Start () {
-	
+
+		musicVol = 3.0f; 
+		source = GetComponent<AudioSource> (); 
+		//source.PlayOneShot (startMusic, musicVol); 
 	}
 	
 	// Update is called once per frame
--- BeatsBoxing/Assets/Scripts/Player.cs
+++ BeatsBoxing/Assets/Scripts/Player.cs
@@ -5,6 +5,11 @@ using System;
 public class Player : LaneActor
 {
 	private int attackTimer; 
+	public AudioClip punch; //the actual punch sound
+	public AudioClip switchLane; //the switching lanes sound
+	public AudioClip takeDamage; //the "oof" sound effect
+	public AudioSource source; //the audioSource object that plays the sounds
+	float audioVol; //volume of any given sound
 
 	void Awake()
     {
@@ -12,6 +17,10 @@ public class Player : LaneActor
         Health = 5;
         Lane = 5;
 		attackTimer = -1; 
+
+		//audio setup
+		audioVol = 3.0f; 
+		source = GetComponent<AudioSource> (); 
     }
 
     public override void Update()
@@ -20,10 +29,12 @@ public class Player : LaneActor
 		if (Input.GetKeyDown(KeyCode.W))
         {
             Lane++;
+			source.PlayOneShot (switchLane, audioVol); 
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             Lane--;
+			source.PlayOneShot (switchLane, audioVol); 
         }
 		if (Input.GetKeyDown (KeyCode.Space) && attackTimer <= 0) 
 		{
@@ -48,11 +59,13 @@ public class Player : LaneActor
     {
 		this.gameObject.transform.GetChild (0).GetComponent<Attack> ().attacking = true; 
 		attackTimer = 42; 
+		source.PlayOneShot (punch, audioVol); 
     }
 
     public void TakeDamage(int damage)
     {
         this.Health -= damage;        
         ScoreManager.Combo = 0;
+		source.PlayOneShot (takeDamage, audioVol); 
     }
 }
