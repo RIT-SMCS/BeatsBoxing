dbcb6c45d7ec646201d3a431980ddce52a5a8efa
Sean Maraia (RIT Student) <spm3713@ad.rit.edu>
2016-04-15 12:56:31 -0400

    Added instructions

--- BeatsBoxing/Assets/Scripts/MainMenuButtons.cs
+++ BeatsBoxing/Assets/Scripts/MainMenuButtons.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MainMenuButtons : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    GameObject instructionsPrefab;
+    GameObject instructionsPanel;
+
+    // Use this for initialization
+    void Start () {
+        instructionsPrefab = Resources.Load("InstructionsPanel") as GameObject;
 	}
 	
 	// Update is called once per frame
@@ -16,5 +20,25 @@ public class MainMenuButtons : MonoBehaviour {
     public void StartGame()
     {
         Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    public void OpenMenu()
+    {
+        if (instructionsPrefab != null && instructionsPanel == null)
+        {
+            instructionsPanel = Instantiate(instructionsPrefab);
+            instructionsPanel.transform.SetParent(this.transform);
+            instructionsPanel.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+            instructionsPanel.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+            instructionsPanel.name = "InstructionsPanel";
+            Button menuBtn = instructionsPanel.transform.FindChild("Text").transform.FindChild("CloseButton").GetComponent<Button>() as Button;
+            menuBtn.onClick.AddListener(delegate () { CloseMenu(); });
+        }
+    }
+
+    public void CloseMenu()
+    {
+        Destroy(this.transform.FindChild("InstructionsPanel").gameObject);
+        instructionsPanel = null;
     }
 }
