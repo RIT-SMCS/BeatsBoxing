c370b3cd9bfe8b0d673570497d0febae189221b6
Sarah Bishop (RIT Student) <sbb8791@ad.rit.edu>
2016-05-10 13:14:38 -0400

    Music set up, soon to be hooked into beat manager

--- BeatsBoxing/Assets/Scripts/MusicManager.cs
+++ BeatsBoxing/Assets/Scripts/MusicManager.cs
@@ -2,21 +2,60 @@
 using System.Collections;
 
 public class MusicManager : MonoBehaviour {
-
+	
 	public AudioClip startMusic; //the initial track
+	public AudioClip musicPhase2; 
+	public AudioClip musicPhase3; 
 	public AudioSource source; //the audioSource
 	float musicVol; //the music volume
-
+	
+	private float scoreBarrier1; //score barriers are for increasing the music
+	private float scoreBarrier2; 
+	
+	private bool switch1; 
+	private bool switch2; 
+	
 	// Use this for initialization
 	void Start () {
-
+		
 		musicVol = 3.0f; 
 		source = GetComponent<AudioSource> (); 
-		source.PlayOneShot (startMusic, musicVol); 
+		source.clip = startMusic; 
+		//source.PlayOneShot (startMusic, musicVol); 
+		source.Play (); 
+		scoreBarrier1 = 100.0f; 
+		scoreBarrier2 = 200.0f; 
+		switch1 = true; 
+		switch2 = true; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (ScoreManager.Score > scoreBarrier2 && switch2) 
+		{
+			source.Pause();
+			int time = source.timeSamples; 
+			//source.PlayOneShot(musicPhase2, musicVol);
+			source.clip = musicPhase3; 
+			source.timeSamples = time; 
+			source.Play(); 
+			//source.clip = musicPhase2; 
+			//source.Play(); 
+			
+			switch2 = false; 
+		}
+		else if(ScoreManager.Score > scoreBarrier1 && switch1)
+		{
+			source.Pause();
+			int time = source.timeSamples; 
+			//source.PlayOneShot(musicPhase2, musicVol);
+			source.clip = musicPhase2; 
+			source.timeSamples = time; 
+			source.Play(); 
+			//source.clip = musicPhase2; 
+			//source.Play(); 
+			
+			switch1 = false; 
+		}
 	}
 }
