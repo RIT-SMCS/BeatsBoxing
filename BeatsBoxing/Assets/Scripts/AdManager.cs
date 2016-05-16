c394cb59bae0bfc386687ca1f7e95599bb45a170
Sarah Bishop (RIT Student) <sbb8791@ad.rit.edu>
2016-05-16 19:00:25 -0400

    In the process of making music sound better, ads are enabled but not tested yet

---
+++ BeatsBoxing/Assets/Scripts/AdManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements; 
+
+public class AdManager : MonoBehaviour {
+
+	void Start ()
+	{
+		Advertisement.Initialize ("1068742", true);
+		
+		StartCoroutine (ShowAdWhenReady ());
+	}
+	
+	IEnumerator ShowAdWhenReady()
+	{
+		while (!Advertisement.IsReady ())
+			yield return null;
+		
+		Advertisement.Show ();
+	}
+}
